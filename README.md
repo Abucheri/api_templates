cf9092fc4a2db3b3fb5fe88ad90b6f7a67785b0b
O'Brien <abucheriobrien@gmail.com>
2023-01-23 13:40:16 -0800

    added endpoints for items and individual items

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # This is a source for my API templates 
+
+## For FastAPI:
+- Import FastAPI.
+- Create an app instance.
+- Write a path operation decorator (like @app.get("/")).
+- Write a path operation function (like def root(): ... above).
+- Run the development server (like uvicorn main:app --reload).
+>> From the docs
+
+## For DRF
--- fastapi/main.py
+++ fastapi/main.py
@@ -3,7 +3,25 @@ from fastapi import FastAPI as fp
 # an object/instance for the API, this is the main point of interaction for the APIs
 app = fp()
 
+items = [{"title": "John Wick", "year": 2014}, {"title": "Black Adam", "year": 2022}]
 
+
+# a decorator to get the root path/url "/" (A "path" is also commonly called an "endpoint" or a "route".)
+# tis decoretor tells FastAPI the function that follows is in charge of handling requests that go to the path (/) using the operation (get)
+# In our case, this decorator tells FastAPI that the function below corresponds to the path / with an operation get. It is the "path operation decorator". - from the docs
 @app.get("/")
-async def root():
-    return {"message":  "Welcome"}
+async def root():  # the path operation function which will be called when it receives a request to the IRL (/) with GET operation
+    return {
+        "message": "Welcome"}  # with this ypo can return a dictionary, list or singular values like strings, int etc...
+
+
+#  getting all items
+@app.get("/items")
+def all_items():
+    return items
+
+
+# getting items by id
+@app.get("/item/{item_id}")
+def individual_item(item_id: int):
+    return items[item_id - 1]
